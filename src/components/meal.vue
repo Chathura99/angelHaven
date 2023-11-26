ddb1b07281d6e446cd0fb3fea0ecd3b044be12a5
Chathura Manohara <chathura.madura11@gmail.com>
2023-11-27 02:20:49 +0530

    meal from back end meal booking

--- src/components/meal.vue
+++ src/components/meal.vue
@@ -1,150 +1,198 @@
-
 <template>
-    <br><br>
-    <div id="backcolour">
-        <h1 style="text-align:center; margin-top: 30px;font-size: 45.8px;margin-bottom: 37px;font-weight: bold">Required Meal Plan</h1>
-  
-        <div class="card-container">
-            <div class="card">
-                <h1 style="font-weight: bold">Breakfast</h1>
-                <p>Rice</p>
-                <p>String Hoppers</p>
-                <p>Bred</p>
-            </div>
-  
-            <div class="card">
-                <h1 style="font-weight: bold">Lunch</h1>
-                <p>Rice and Curry</p>
-                 <p>Fried rice</p>
+  <br><br>
+  <div id="backcolour">
+    <h1 style="text-align:center; margin-top: 30px;font-size: 45.8px;margin-bottom: 37px;font-weight: bold">
+      Required Meal Plan
+    </h1>
 
-            </div>
-  
-            <div class="card">
-                <h1 style="font-weight: bold">Dinner</h1>
-                <p>Rice</p>
-                <p>String Hoppers</p>
-                <p>Bred</p>
-            </div>
-  
-        </div>
+    <div class="card-container">
+      <div class="card">
+        <h1 style="font-weight: bold">Breakfast</h1>
+        <p v-for="item in items.Breakfast" :key="item">{{ item }}</p>
+        <!-- <input v-model="newBreakfastItem" id="newItem" class="form-control" placeholder="Enter new item" required>
+        <button @click="addItem('Breakfast')" class="btn btn-primary"
+          style="margin-top: 20px; margin-bottom: 30px; color: black; background: white; border: 1px solid #f0d8b6; width: 100%;"><b>Add
+            Item</b></button> -->
+      </div>
+
+      <div class="card">
+        <h1 style="font-weight: bold">Lunch</h1>
+        <p v-for="item in items.Lunch" :key="item">{{ item }}</p>
+      </div>
+
+      <div class="card">
+        <h1 style="font-weight: bold">Dinner</h1>
+        <p v-for="item in items.Dinner" :key="item">{{ item }}</p>
+      </div>
     </div>
-  </template>
-  
+  </div>
+</template>
+
 <script>
-    export default {
-        name: 'meal'   
-    }
+import { collection, getDocs, doc, updateDoc } from 'firebase/firestore';
+import db from '../firebase/init.js';
+
+export default {
+  data() {
+    return {
+      items: {
+        Breakfast: [],
+        Lunch: [],
+        Dinner: [],
+      },
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      getDocs(collection(db, 'meal_plan'))
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            this.items = doc.data();
+            console.log(this.items);
+          });
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
+    },
+    addItem(mealType) {
+      // Add the new item to the corresponding meal type array
+      const newItem = this[`new${mealType}Item`];
+
+      // Update the data in Firestore for the specific meal type
+      const mealPlanRef = doc(db, 'meal_plan', 'your_document_id'); // Replace 'your_document_id' with your actual document ID
+
+      // Create an object with the specific field to update
+      const updateObject = {};
+      updateObject[mealType] = this.items[mealType];
+
+      updateDoc(mealPlanRef, updateObject)
+        .then(() => {
+          console.log('Item added successfully');
+        })
+        .catch(error => {
+          console.error('Error updating document:', error);
+        });
+
+      // Clear the input field
+      this[`new${mealType}Item`] = '';
+    },
+
+  },
+};
 </script>
 
-  <style scoped>
+<style scoped>
+.card {
+  max-width: 300px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  border-radius: 12px;
+}
+
+.price {
+  color: grey;
+  font-size: 22px;
+}
+
+.card button {
+  border: none;
+  outline: 0;
+  padding: 12px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+}
+
+.card button:hover {
+  opacity: 0.7;
+}
+
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.card {
+  flex: 1 0 30%;
+  margin: 10px;
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+  text-align: center;
+  padding: 10px;
+  box-sizing: border-box;
+}
+
+.card img {
+  width: 100%;
+  height: auto;
+}
+
+@media (max-width: 768px) {
   .card {
-    max-width: 300px;
-    margin: auto;
-    text-align: center;
-    font-family: arial;
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-    border-radius: 12px;
+    flex-basis: 45%;
   }
-  
-  .price {
-    color: grey;
-    font-size: 22px;
-  }
-  
-  .card button {
-    border: none;
-    outline: 0;
-    padding: 12px;
-    color: white;
-    background-color: #000;
-    text-align: center;
-    cursor: pointer;
-    width: 100%;
-    font-size: 18px;
-  }
-  
-  .card button:hover {
-    opacity: 0.7;
-  }
-  
-  .card-container {
+
+  .card-container[data-v-99a59d87] {
     display: flex;
     flex-wrap: wrap;
-  }
-  
-  .card {
-    flex: 1 0 30%; 
-    margin: 10px;
-    border: 1px solid #ccc;
-    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
-    text-align: center;
-    padding: 10px;
-    box-sizing: border-box;
-  }
-  
-  .card img {
-    width: 100%;
-    height: auto;
-  }
-  
-  @media (max-width: 768px) {
-    .card {
-      flex-basis: 45%; 
-    }
-  
-    .card-container[data-v-99a59d87] {
-      display: flex;
-      flex-wrap: wrap;
-      flex-direction: column-reverse;
-      align-items: center;
+    flex-direction: column-reverse;
+    align-items: center;
   }
 
   .card-container[data-v-79d2af26] {
     margin-left: 8%;
+  }
 }
-  }
-  
-  .card-container[data-v-99a59d87] {
-      display: flex;
-      flex-wrap: wrap;
-      align-items: center;
-      flex-direction: row;
-      align-content: center;
-      justify-content: center;
-  }
-  
-  .card img[data-v-99a59d87] {
-      width: 80%;
-      HEIGHT: 80%;
-      MARGIN-LEFT: 11%;
-  }
-  
-  .card:hover {
-    opacity: 0.7;
-    background:rgba(240,216,182,255);
-  }
-  
-  h1, .h1 {
-      padding-top: 17px;
-  }
-  
-  .card {
-    margin-bottom: 50px;
-  }
-  
-  #backcolour{
-    background: white;
-  }
 
-  @media (min-width: 1200px) {
-    .card-container{
+.card-container[data-v-99a59d87] {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  flex-direction: row;
+  align-content: center;
+  justify-content: center;
+}
+
+.card img[data-v-99a59d87] {
+  width: 80%;
+  HEIGHT: 80%;
+  MARGIN-LEFT: 11%;
+}
+
+.card:hover {
+  opacity: 0.7;
+  background: rgba(240, 216, 182, 255);
+}
+
+h1,
+.h1 {
+  padding-top: 17px;
+}
+
+.card {
+  margin-bottom: 50px;
+}
+
+#backcolour {
+  background: white;
+}
+
+@media (min-width: 1200px) {
+  .card-container {
     display: flex;
     flex-wrap: nowrap;
     flex-direction: row;
     justify-content: space-evenly;
-}
-    
   }
 
-
-  </style>
+}
+</style>
--- src/components/meal1.vue
+++ src/components/meal1.vue
@@ -2,7 +2,7 @@
   <div class="container" style="background: #f8f8f8; border-radius: 10px;">
     <div class="section-title">
       <br>
-      <h3 class="faqsection" style="font-size: 45.8px; font-weight: bold; margin-top: 40px;">Donate</h3>
+      <h3 class="faqsection" style="font-size: 45.8px; font-weight: bold; margin-top: 40px;">Booking (Donate)</h3>
     </div>
     <div class="row">
       <div class="col-md-6 offset-md-3">
@@ -16,24 +16,41 @@
         </div>
         <div class="mb-3">
           <label for="contact" class="form-label">Contact Number</label>
-          <input v-model="contact" type="number" id="contact" class="form-control" placeholder="Enter Contact Number"
-            required>
+          <input v-model="contact" type="number" id="contact" class="form-control" placeholder="Enter Contact Number" required>
         </div>
         <div class="mb-3">
           <label for="dateInput" class="form-label">Date</label>
           <input type="date" v-model="dateInput" id="dateInput" class="form-control" required>
         </div>
-        <div class="mb-3">
-          <label for="Meal" class="form-label">Meal Type</label>
-          <input v-model="Meal" id="Meal" class="form-control" placeholder="Enter Meal Type" required>
+
+        <!-- <div class="mb-3">
+          <button @click="searchBookings" class="btn btn-secondary"
+            style="margin-top: 20px; color: white; background: #6c757d; border: 1px solid #6c757d; width: 100%;"><b>Search Bookings</b></button>
+        </div> -->
+
+        <div v-if="bookings && bookings.length > 0" class="mt-4">
+          <h4 class="mb-3">Bookings for {{ dateInput }}</h4>
+          <ul>
+            <li v-for="(booking, index) in bookings" :key="index">
+              <strong>{{ booking.firstName }} {{ booking.lastName }}</strong> - {{ booking.meal }}
+            </li>
+          </ul>
         </div>
-        <div class="mb-3">
-          <label for="Position" class="form-label">Position</label>
-          <input v-model="Position" type="number" id="Position" class="form-control" placeholder="Enter Position"
-            required>
+        <div v-else class="mt-4">
+          <p>No bookings found for the selected date.</p>
         </div>
 
-        <button @click="createUser" class="btn btn-primary"
+        <div class="mb-3">
+          <label for="Meal" class="form-label">Meal Type</label>
+          <select v-model="meal" id="Meal" class="form-select" required>
+            <option value="" disabled>Select Meal Type</option>
+            <option value="breakfast">Breakfast</option>
+            <option value="lunch">Lunch</option>
+            <option value="dinner">Dinner</option>
+          </select>
+        </div>
+
+        <button @click="createBooking" class="btn btn-primary"
           style="margin-top: 20px; margin-bottom: 30px; color: black; background: white; border: 1px solid #f0d8b6; width: 100%;"><b>Submit</b></button>
 
       </div>
@@ -42,33 +59,64 @@
 </template>
 
 <script>
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDoc, doc, query, where, getDocs } from "firebase/firestore";
 import db from '../firebase/init.js';
-
+import Swal from 'sweetalert2';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 export default {
   data() {
     return {
       firstName: '',
       lastName: '',
       contact: '',
-      dateInput: '',
-      Meal: '',
-      Position: '',
+      dateInput: new Date().toISOString().substr(0, 10),
+      meal: 'Breakfast',
+      position: '',
       userCreated: false,
-      userData: {}
+      userData: {},
+      bookings: [],
     };
   },
-
+  watch: {
+    // Watch for changes in dateInput and call searchBookings
+    dateInput: 'searchBookings',
+  },
+  mounted() {
+    // Fetch the current user data when the component is mounted
+    this.fetchUserData();
+  },
   methods: {
-    async createUser() {
+    async createBooking() {
       const colRef = collection(db, 'booking');
+
+      // Check if a booking already exists for the specified dateInput and meal
+      const existingBookingQuery = query(
+        colRef,
+        where('dateInput', '==', this.dateInput),
+        where('meal', '==', this.meal)
+      );
+
+      const existingBookingSnapshot = await getDocs(existingBookingQuery);
+
+      if (!existingBookingSnapshot.empty) {
+        // A booking already exists for the specified dateInput and meal
+        Swal.fire({
+          icon: 'error',
+          title: 'Error!',
+          text: 'Booking already exists for this date and meal.',
+        });
+        return; // Stop further execution
+      }
+
       const dataObj = {
         firstName: this.firstName,
         lastName: this.lastName,
         contact: this.contact,
         dateInput: this.dateInput,
-        Meal: this.Meal,
-        Position: this.Position
+        meal: this.meal,
+        position: this.position,
+        status: 'Pending',
+        uid:this.uid
       };
 
       const docRef = await addDoc(colRef, dataObj);
@@ -77,21 +125,78 @@ export default {
       this.userCreated = true;
       this.userData = dataObj;
 
-      alert('Form submitted successfully!'); // Display the alert message
+      Swal.fire({
+        icon: 'success',
+        title: 'Success!',
+        text: 'Booking successfully!'
+      });
 
       // Clear form data after submission
       this.firstName = '';
       this.lastName = '';
       this.contact = '';
       this.dateInput = '';
-      this.Meal = '';
-      this.Position = '';
-    }
-  },
+      this.meal = '';
+      this.position = '';
+      this.uid = '';
+    },
+    async fetchUserData() {
+      const auth = getAuth();
+      onAuthStateChanged(auth, async (user) => {
+        if (user) {
+          // User is logged in, fetch user data from Firestore
+          const userDocRef = doc(db, 'users', user.uid);
+          try {
+            const userDoc = await getDoc(userDocRef);
+            if (userDoc.exists()) {
+              // Set the firstName and lastName based on the fetched user data
+              const userData = userDoc.data();
+              this.firstName = userData.firstName;
+              this.lastName = userData.lastName;
+              this.contact = userData.contact;
+              this.uid = userData.uid;
+            }
+          } catch (error) {
+            console.error('Error fetching user data:', error);
+          }
+        }
+      });
+    },
+    async searchBookings() {
+      const colRef = collection(db, 'booking');
 
-  created() {
-    // Optional: You can call createUser on component creation if needed.
-    // this.createUser();
-  }
+      // Fetch bookings for the selected dateInput
+      const bookingsQuery = query(
+        colRef,
+        where('dateInput', '==', this.dateInput)
+      );
+
+      try {
+        const bookingsSnapshot = await getDocs(bookingsQuery);
+
+        if (bookingsSnapshot.empty) {
+          this.bookings = []; // Clear previous bookings
+          // Swal.fire({
+          //   icon: 'info',
+          //   title: 'Info',
+          //   text: 'No bookings found for the selected date.',
+          // });
+          return;
+        }
+
+        // Extract and set booking details
+        this.bookings = bookingsSnapshot.docs.map((doc) => doc.data());
+        console.log('Bookings:', this.bookings);
+
+      } catch (error) {
+        console.error('Error fetching bookings:', error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error!',
+          text: 'Failed to fetch bookings. Please try again.',
+        });
+      }
+    },
+  },
 };
 </script>
